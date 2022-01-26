8bcd32ebce3ad7aaef6a55ea64e147191d163594
Donn <me@donn.website>
2022-01-26 14:28:08 +0200

    Better Error Handling

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,3 +1,4 @@
+#include "python.hpp"
 #include "util.hpp"
 
 #include <boost/format.hpp>
@@ -11,47 +12,9 @@
 #include <vector>
 
 using boost::format;
-
-// Return the current Python error and backtrace as a string, or throw
-// an exception if there was none.
-std::string python_error_string() {
-    using namespace boost::python;
-
-    PyObject *ptype = nullptr;
-    PyObject *pvalue = nullptr;
-    PyObject *ptraceback = nullptr;
-
-    // Fetch the exception information. If there was no error ptype will be set
-    // to null. The other two values might set to null anyway.
-    PyErr_Fetch(&ptype, &pvalue, &ptraceback);
-    if (ptype == nullptr) {
-        throw std::runtime_error(
-            "A Python error was detected but when we called "
-            "PyErr_Fetch() it returned null indicating that "
-            "there was no error.");
-    }
-
-    // Sometimes pvalue is not an instance of ptype. This converts it. It's
-    // done lazily for performance reasons.
-    PyErr_NormalizeException(&ptype, &pvalue, &ptraceback);
-    if (ptraceback != nullptr) {
-        PyException_SetTraceback(pvalue, ptraceback);
-    }
-
-    // Get Boost handles to the Python objects so we get an easier API.
-    handle<> htype(ptype);
-    handle<> hvalue(allow_null(pvalue));
-    handle<> htraceback(allow_null(ptraceback));
-
-    // Import the `traceback` module and use it to format the exception.
-    object traceback = import("traceback");
-    object format_exception = traceback.attr("format_exception");
-    object formatted_list = format_exception(htype, hvalue, htraceback);
-    object formatted = str("\n").join(formatted_list);
-    return extract< std::string >(formatted);
-}
-
 boost::python::object moduleDictRef;
+
+auto traceback = PyErr_Print;
 
 extern "C" int RunPythonCommand(
     ClientData cdata,
@@ -82,8 +45,8 @@ extern "C" int RunPythonCommand(
         return TCL_OK;
 
     } catch (boost::python::error_already_set &e) {
-        boost::python::handle_exception();
-        std::cerr << python_error_string() << std::endl;
+        handle_exception();
+        traceback();
         return TCL_ERROR;
     }
 }
@@ -141,8 +104,8 @@ extern "C" int DLLEXPORT Pyinter_Init(Tcl_Interp *interp) {
             Tcl_CreateObjCommand(interp, name, RunPythonCommand, NULL, NULL);
         }
     } catch (boost::python::error_already_set &e) {
-        boost::python::handle_exception();
-        std::cerr << python_error_string() << std::endl;
+        handle_exception();
+        traceback();
         return TCL_ERROR;
     } catch (std::runtime_error &e) {
         std::cerr << e.what() << std::endl;
